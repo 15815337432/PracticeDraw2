126fdbc020ffe03fe1d5971e2e9c0d849d0f11f1
wanglianghai <1540446221@qq.com>
2017-08-16 11:04:18 +0800

    finish

--- app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice14MaskFilterView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice14MaskFilterView.java
@@ -3,6 +3,7 @@ package com.hencoder.hencoderpracticedraw2.practice;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.BlurMaskFilter;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Path;
@@ -15,6 +16,10 @@ import com.hencoder.hencoderpracticedraw2.R;
 public class Practice14MaskFilterView extends View {
     Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
     Bitmap bitmap;
+    private BlurMaskFilter mBlurMaskFilter = new BlurMaskFilter(50, BlurMaskFilter.Blur.NORMAL);
+    private BlurMaskFilter mBlurMaskFilter2 = new BlurMaskFilter(50, BlurMaskFilter.Blur.INNER);
+    private BlurMaskFilter mBlurMaskFilter3 = new BlurMaskFilter(50, BlurMaskFilter.Blur.OUTER);
+    private BlurMaskFilter mBlurMaskFilter4 = new BlurMaskFilter(50, BlurMaskFilter.Blur.SOLID);
 
     public Practice14MaskFilterView(Context context) {
         super(context);
@@ -40,15 +45,19 @@ public class Practice14MaskFilterView extends View {
         // 用 Paint.setMaskFilter 来设置不同的 BlurMaskFilter
 
         // 第一个：NORMAL
+        paint.setMaskFilter(mBlurMaskFilter);
         canvas.drawBitmap(bitmap, 100, 50, paint);
 
         // 第二个：INNER
+        paint.setMaskFilter(mBlurMaskFilter2);
         canvas.drawBitmap(bitmap, bitmap.getWidth() + 200, 50, paint);
 
         // 第三个：OUTER
+        paint.setMaskFilter(mBlurMaskFilter3);
         canvas.drawBitmap(bitmap, 100, bitmap.getHeight() + 100, paint);
 
         // 第四个：SOLID
+        paint.setMaskFilter(mBlurMaskFilter4);
         canvas.drawBitmap(bitmap, bitmap.getWidth() + 200, bitmap.getHeight() + 100, paint);
     }
 }
